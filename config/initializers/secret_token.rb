c584e2264a22983b6a766df8a4e011fe7261501f
Anjana <anjana@anjana-VirtualBox.(none)>
2015-01-17 15:59:59 +0530

    new ones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipeBook::Application.config.secret_token = 'fd5b08412b41dd6b291622f2cfcd7196eb915bb40b29ec9f3477012c4e2c8673103929930e841d8e56a720b732ff22d5eca0beeb7ee9e32dc0afc21c4a41e360'
---
+++ test/unit/helpers/welcome_mailer_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class WelcomeMailerHelperTest < ActionView::TestCase
+end
